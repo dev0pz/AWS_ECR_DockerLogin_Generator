91c522acd8095c767a08d8d2499c62174f88757f
Developer <Developer@MacBookAir.local>
2019-11-17 16:26:50 -0300

    uploading for andrew

--- gimme_dockerlogin.py
+++ gimme_dockerlogin.py
@@ -1,7 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError, WaiterError
 import base64
-import subprocess
 
 session = boto3.Session(
 aws_access_key_id = 'xxxxxx',
@@ -14,10 +13,4 @@ resp = ecr.get_authorization_token()
 auth_code = base64.b64decode(resp['authorizationData'][0]['authorizationToken']).split(':')[1]
 docker_login = 'docker login -u AWS -p ' + auth_code + ' ' + resp['authorizationData'][0]['proxyEndpoint']
 
-try:
-    run_login = subprocess.check_output(docker_login, stderr=subprocess.STDOUT, shell=True)
-except subprocess.CalledProcessError as e:
-    print("Exception: %s\n" % e)
-    print("Exception Output: %s\n" % e.output)
-else:
-    print run_login
+print docker_login
